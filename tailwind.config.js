7a7808e719333a9b75ca997681bd1938dd281d0e
kimcy <re2@naver.com>
2023-07-11 19:05:14 +0900

    add. main 화면 개발

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  prefix: 'tw-',
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/line-clamp'),
+  ],
+};
