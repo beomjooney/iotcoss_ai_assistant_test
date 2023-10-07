314a504e09712abf04715ccd6c45fd66d113be1a
@10143918 <re2@naver.com>
2023-10-07 14:52:01 +0900

    add. replay added

--- src/stories/components/CommunityCardReply/index.tsx
+++ src/stories/components/CommunityCardReply/index.tsx
@@ -6,6 +6,10 @@ import { jobColorKey } from 'src/config/colors';
 import { User } from 'src/models/user';
 import Tooltip from '../Tooltip';
 import Grid from '@mui/material/Grid';
+import { Textfield, Button } from 'src/stories/components';
+import { useRef, useState } from 'react';
+import { useSessionStore } from 'src/store/session';
+const { logged } = useSessionStore.getState();
 
 export interface CommunityCardReplyProps {
   /** 댓글 */
@@ -20,41 +24,104 @@ export interface CommunityCardReplyProps {
 
 const cx = classNames.bind(styles);
 
+function timeForToday(value) {
+  const today = new Date();
+  const timeValue = new Date(value);
+
+  const betweenTime = Math.floor((today.getTime() - timeValue.getTime()) / 1000 / 60);
+  if (betweenTime < 1) return '방금 전';
+  if (betweenTime < 60) {
+    return `${betweenTime}분 전`;
+  }
+
+  const betweenTimeHour = Math.floor(betweenTime / 60);
+  if (betweenTimeHour < 24) {
+    return `${betweenTimeHour}시간 전`;
+  }
+
+  const betweenTimeDay = Math.floor(betweenTime / 60 / 24);
+  if (betweenTimeDay < 365) {
+    return `${betweenTimeDay}일 전`;
+  }
+
+  return `${Math.floor(betweenTimeDay / 365)}년전`;
+}
+
 const CommunityCardReply = ({ reply, writer, className, memberId, onReplyDeleteSubmit }: CommunityCardReplyProps) => {
   // const { jobGroupName, jobGroup } = writer;
   // const chipColor = jobColorKey(jobGroup);
+  const textInput = useRef(null);
+  let [isOpen, setIsOpened] = useState(false);
+
+  const onReplySubmit = () => {
+    setIsOpened(!isOpen);
+    // if (logged) {
+    //   onSaveReply({
+    //     postNo: postNo,
+    //     data: {
+    //       body: text,
+    //     },
+    //   });
+    //   textInput.current.value = '';
+    //   setReplyCount(replyCount => replyCount + 1);
+    // } else {
+    //   alert('로그인 후 댓글을 입력할 수 있습니다.');
+    // }
+  };
 
   // TODO 좋아요 여부 필드 수정 필요
 
   return (
     <div className={cx('community-board-reply-container', 'row', className)}>
-      <div className="tw-grid  tw-grid-cols-12  tw-flex tw-justify-content-center tw-items-center tw-space-x-4 tw-my-2 tw-gap-2">
+      <div className="tw-grid  tw-grid-cols-12  tw-flex tw-items-center  tw-mt-2 tw-gap-2">
         {/* {board.postNo} */}
-        <div className="tw-col-span-1 tw-flex tw-items-end tw-justify-end">
+        <div className="tw-col-span-1 tw-flex tw-items-end tw-justify-center">
           <img src={reply?.imageUrl} alt={'image'} className={cx('rounded-circle', 'profile-image', 'tw-h-10')} />
         </div>
-        <div className="tw-col-span-1">
-          <div className="tw-font-bold tw-text-lg tw-text-black">{reply?.nickname}</div>
+        <div className="tw-col-span-3 tw-text-left tw-flex tw-items-center">
+          <div className="tw-font-bold tw-text-lg tw-text-black">{reply?.nickname} </div>
+          <div className="tw-text-[11px] tw-text-gray-400 tw-pl-4">{timeForToday(reply.createAt)}</div>
         </div>
-        <div className="tw-col-span-10">
-          <div className="tw-text-sm">{reply.createAt.toString().split(' ')[0]}</div>
-        </div>
+        <div className="tw-col-span-7"></div>
       </div>
 
-      <div className="tw-grid tw-grid-cols-12 tw-gap-4 tw-items-start tw-justify-center">
+      <div className="tw-grid tw-grid-cols-12 tw-items-start tw-justify-center">
         <div className="tw-col-span-1"></div>
-        <div className="tw-col-span-11">
-          <div className={cx('reply-wrap', 'col-md-7', 'tw-text-sm')}>{reply.body}</div>
+        <div className="tw-col-span-11 tw-py-3">
+          <div className="tw-text-sm">{reply.body}</div>
         </div>
         <div className="tw-col-span-1"></div>
       </div>
-      <div className="tw-grid tw-grid-cols-12 tw-gap-4 tw-items-start tw-justify-center">
+      {/* <div className="tw-grid tw-grid-cols-12 tw-items-start tw-justify-center">
         <div className="tw-col-span-1"></div>
-        <div className="tw-col-span-11">
-          <div className={cx('reply-wrap', 'col-md-7', 'tw-text-[12px]', 'tw-text-gray-400')}>댓글쓰기</div>
+        <div className="tw-col-span-11 tw-py-3">
+          <button className={cx('tw-text-[12px]', 'tw-text-gray-400')} onClick={() => onReplySubmit()}>
+            댓글쓰기
+          </button>
         </div>
         <div className="tw-col-span-1"></div>
-      </div>
+      </div> */}
+      {isOpen && (
+        <div className="tw-grid tw-grid-cols-12 tw-items-center tw-justify-center">
+          <div className="tw-col-span-1"></div>
+          <div className="tw-col-span-10">
+            <div className={cx('tw-text-[12px]', 'tw-text-gray-400')}>
+              <Textfield defaultValue="" placeholder="댓글을 입력하세요." ref={textInput} />
+            </div>
+          </div>
+          <div className="tw-col-span-1">
+            <div>
+              <button
+                className="tw-bg-gray-400 tw-text-white tw-py-[10px] tw-px-6  tw-ml-2 tw-rounded-md tw-text-sm"
+                onClick={() => onReplySubmit(reply.postNo, textInput.current.value)}
+              >
+                입력
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       {/* <div className={cx('profile-wrap', 'col-md-1')}>
         <img
           src={`${process.env['NEXT_PUBLIC_GENERAL_IMAGE_URL']}/images/${reply.author?.profileImageUrl}`}
