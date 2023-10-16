ebdabd13a8c356e8dc9a9ff06874ef53f4376d03
dev_dog <devdogstudio@gmail.com>
2023-10-16 21:24:15 +0900

    added. monthly quiz

--- src/templates/Monthly/Quiz/index.tsx
+++ src/templates/Monthly/Quiz/index.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState, useRef } from 'react';
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
 import { useRouter } from 'next/router';
-import { Toggle } from 'src/stories/components';
+import { Toggle, Pagination } from 'src/stories/components';
 import { paramProps } from 'src/services/seminars/seminars.queries';
-import { useMonthlyRanking, useQuizzesAnswers, quizzesAnswersParamProps } from 'src/services/monthly/monthly.queries';
-import { MonthlyQuizzesResponse, MonthlyRankingResponse } from 'src/models/monthly';
+import { useMonthlyRanking, useQuizzesAnswers } from 'src/services/monthly/monthly.queries';
+import { MonthlyQuizzesResponse, MonthlyRankingResponse, QuizzesAnswersResponse } from 'src/models/monthly';
 
 import Grid from '@mui/material/Grid';
 import Box from '@mui/system/Box';
@@ -23,15 +23,13 @@ export function MonthlyQuizTemplate() {
   const router = useRouter();
   const [active, setActive] = useState(0);
   const [page, setPage] = useState(1);
-  const [totalPage, setTotalPage] = useState(1);
   const [params, setParams] = useState<paramProps>({ page });
   const [monthlyQuizzesContents, setMonthlyQuizzesContents] = useState<MonthlyQuizzesResponse>();
   const [monthlyRankingContents, setMonthlyRankingContents] = useState<MonthlyRankingResponse>();
+  const [totalPage, setTotalPage] = useState(1);
   const [totalElements, setTotalElements] = useState(0);
-  let [quizzesAnswersContents, setQuizzesAnswersContents] = useState<quizzesAnswersParamProps>();
+  let [quizzesAnswersContents, setQuizzesAnswersContents] = useState<QuizzesAnswersResponse>();
   let [quizSequence, setQuizSequence] = useState<number>(0);
-
-  quizSequence = 17;
 
   // 퀴즈 데이터
   const { isFetched: isMonthlyRankingFetched } = useMonthlyRanking(data => {
@@ -41,14 +39,13 @@ export function MonthlyQuizTemplate() {
 
   // 답변 데이터
   const { isFetched: isQuizzesAnswersFetched, refetch } = useQuizzesAnswers(quizSequence, params, data => {
-    setQuizzesAnswersContents(data?.contents);
+    setQuizzesAnswersContents(data);
     setTotalPage(data?.totalPages);
     setTotalElements(data?.totalElements);
   });
 
   useEffect(() => {
     setParams({
-      //...params,
       page,
     });
   }, [page]);
@@ -56,8 +53,6 @@ export function MonthlyQuizTemplate() {
   useEffect(() => {
     if (quizSequence > 0) refetch();
   }, [quizSequence]);
-
-  console.log(quizzesAnswersContents);
 
   const handleIconButton = (event: React.MouseEvent<HTMLElement>) => {};
 
@@ -91,7 +86,7 @@ export function MonthlyQuizTemplate() {
               <Toggle
                 name={`HOT ${index + 1}`}
                 label={`HOT ${index + 1}`}
-                key={values?.quizSequence}
+                key={`hot-${index}`}
                 value={values?.quizSequence}
                 variant="small"
                 type="tabButton"
@@ -111,20 +106,29 @@ export function MonthlyQuizTemplate() {
         {monthlyRankingContents?.quizzes?.map(
           (values, index: number) =>
             active === index && (
-              <div>
+              <div key={`quiz-${index}`}>
                 <div className="tw-pt-10" key={values.quizSequence}>
                   {values?.recommendJobGroupNames?.map(jobGroupNamesValues => (
-                    <span className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-blue-100 tw-text-sm tw-font-light tw-text-blue-600">
+                    <span
+                      key={`jobGroup-${jobGroupNamesValues}`}
+                      className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-blue-100 tw-text-sm tw-font-light tw-text-blue-600"
+                    >
                       {jobGroupNamesValues}
                     </span>
                   ))}
                   {values?.recommendLevels?.map(recommendLevelsValues => (
-                    <span className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-red-100 tw-text-sm tw-font-light tw-text-red-600">
+                    <span
+                      key={`level-${recommendLevelsValues}`}
+                      className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-red-100 tw-text-sm tw-font-light tw-text-red-600"
+                    >
                       레벨 {recommendLevelsValues}
                     </span>
                   ))}
                   {values?.recommendJobNames?.map(recommendJobNamesValues => (
-                    <span className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-gray-300 tw-text-sm tw-font-light tw-text-gray-600">
+                    <span
+                      key={`job-${recommendJobNamesValues}`}
+                      className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-bg-gray-300 tw-text-sm tw-font-light tw-text-gray-600"
+                    >
                       {recommendJobNamesValues}
                     </span>
                   ))}
@@ -182,78 +186,78 @@ export function MonthlyQuizTemplate() {
         )}
 
         <article>
-          <div className={cx('answer-area tw-pt-5')}>
-            <div className={cx('content-wrap tw-rounded tw-bg-white border')}>
-              <div className="tw-flex p-3 tw-m-1 tw-px-3 tw-py-0.5">
-                <div className="tw-w-1/6">
-                  <span className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-text-sm tw-font-light">
-                    20.06.01
-                  </span>
-                </div>
-                <div className="tw-w-3/4">
-                  <span className="tw-flex tw-text-black tw-text-sm">
-                    <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
-                    <span className="tw-leading-9 tw-pl-2 tw-font-bold">개발열공러님</span>
-                  </span>
-                </div>
-                <div className="tw-w-3/4 tw-text-right">
-                  <IconButton
-                    aria-label="more"
-                    id="long-button"
-                    size="small"
-                    aria-haspopup="true"
-                    onClick={e => handleIconButton(e)}
-                  >
-                    <AssignmentOutlinedIcon />
-                  </IconButton>
-                  <IconButton
-                    aria-label="more"
-                    id="long-button"
-                    size="small"
-                    aria-haspopup="true"
-                    onClick={e => handleIconButton(e)}
-                  >
-                    <StarBorderIcon />
-                  </IconButton>
+          {isQuizzesAnswersFetched &&
+            (quizzesAnswersContents?.contents.length > 0 ? (
+              quizzesAnswersContents?.contents.map((values, index: number) => {
+                return (
+                  <div key={`content-${index}`}>
+                    <div className={cx('answer-area tw-pt-5')}>
+                      <div className={cx('content-wrap tw-rounded tw-bg-white border')}>
+                        <div className="tw-flex p-3 tw-m-1 tw-px-3 tw-py-0.5">
+                          <div className="tw-w-1/6">
+                            <span className="tw-inline-flex tw-rounded tw-items-center tw-m-1 tw-px-3 tw-py-0.5 tw-text-sm tw-font-light">
+                              {values?.createdAt.slice(2, 10)}
+                            </span>
+                          </div>
+                          <div className="tw-w-3/4">
+                            <span className="tw-flex tw-text-black tw-text-sm">
+                              <Avatar sx={{ width: 32, height: 32 }} src={values?.profileImageUrl}></Avatar>
+                              <span className="tw-leading-9 tw-pl-2 tw-font-bold">{values?.nickname}</span>
+                            </span>
+                          </div>
+                          <div className="tw-w-3/4 tw-text-right">
+                            <IconButton
+                              aria-label="more"
+                              id="long-button"
+                              size="small"
+                              aria-haspopup="true"
+                              onClick={e => handleIconButton(e)}
+                            >
+                              <AssignmentOutlinedIcon className="tw-mr-1 tw-w-5" />
+                              <span className="tw-text-sm">{values?.replyCount ?? 0}</span>
+                            </IconButton>
+                            <IconButton
+                              aria-label="more"
+                              id="long-button"
+                              size="small"
+                              aria-haspopup="true"
+                              onClick={e => handleIconButton(e)}
+                            >
+                              <StarBorderIcon className="tw-mr-1 tw-w-5" />
+                              <span className="tw-text-sm">{values?.onePickCount ?? 0}</span>
+                            </IconButton>
 
-                  <IconButton
-                    aria-label="more"
-                    id="long-button"
-                    size="small"
-                    aria-haspopup="true"
-                    onClick={e => handleIconButton(e)}
-                  >
-                    <ContentCopyIcon />
-                  </IconButton>
+                            <IconButton
+                              aria-label="more"
+                              id="long-button"
+                              size="small"
+                              aria-haspopup="true"
+                              onClick={e => handleIconButton(e)}
+                            >
+                              <FavoriteBorderIcon className="tw-mr-1 tw-w-5" />
+                              <span className="tw-text-sm">{values?.likeCount ?? 0}</span>
+                            </IconButton>
+                          </div>
+                        </div>
+                        <div className="tw-flex p-3 tw-m-1 tw-px-3 tw-py-0.5">
+                          <span className="tw-flex tw-text-black tw-text-sm tw-pl-[120px]">{values?.postAnswer}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div>
+                <div className={cx('answer-area tw-p-[100px] tw-text-center')}>
+                  <span className="tw-font-bold tw-text-black">해당 퀴즈를 풀고 답변을 확인해보세요!</span>
                 </div>
               </div>
-              <div className="tw-flex p-3 tw-m-1 tw-px-3 tw-py-0.5">
-                <span className="tw-flex tw-text-black tw-text-sm tw-pl-[120px]">
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet, possimus! Aliquid ipsum consectetur
-                  cumque magni quo expedita, animi ex veritatis eaque aspernatur ad, laudantium dignissimos fugit dicta
-                  dolore! Eos, odio! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab quos earum vero
-                  incidunt necessitatibus porro molestiae magnam, sit corrupti dolorem nulla repellat facere tempora
-                  ipsa! Assumenda ipsa temporibus sapiente atque. Lorem ipsum, dolor sit amet consectetur adipisicing
-                  elit. Aperiam expedita neque ipsum magni quasi, perspiciatis nostrum ipsa sed, eaque itaque odio
-                  doloribus accusantium! Omnis perferendis a laboriosam quam architecto quae? Lorem ipsum dolor sit amet
-                  consectetur adipisicing elit. Consectetur, eum! Obcaecati corporis molestias maiores quo id temporibus
-                  laudantium iste labore hic saepe. Delectus cumque cupiditate et fugiat quam, sint tempora! Lorem ipsum
-                  dolor sit amet consectetur adipisicing elit. Ipsam dicta at odit ipsa numquam? Placeat, quidem
-                  excepturi quaerat, maxime voluptates aperiam suscipit incidunt illum recusandae provident, quibusdam
-                  nemo ducimus veniam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iusto optio
-                  ratione, nulla molestias quasi accusantium odio architecto sint maiores quibusdam magni, debitis saepe
-                  sed velit eveniet voluptatem ipsam laborum!
-                </span>
-              </div>
-            </div>
+            ))}
+          <div className="tw-mt-10">
+            <Pagination page={page} setPage={setPage} total={totalPage} />
           </div>
         </article>
-
-        {/* <article>
-          <div className={cx('answer-area tw-p-[100px] tw-text-center')}>
-            <span className="tw-font-bold tw-text-black">해당 퀴즈를 풀고 답변을 확인해보세요!</span>
-          </div>
-        </article> */}
       </div>
     </div>
   );
