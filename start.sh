3164601cd878a1692f0b9a4df1abbe33713ad5a8
root <root@ip-172-31-36-60.ap-northeast-2.compute.internal>
2024-07-27 17:11:45 +0900

    fix. start.sh

--- start.sh
+++ start.sh
@@ -2,16 +2,16 @@
 
 git pull origin main
 yarn build
-pm2 delete pnpm build
+#pm2 delete pnpm build
 
 echo "Stopping all PM2 processes..."
-pm2 stop all
+#pm2 stop all
 
 echo "Deleting all PM2 processes..."
-pm2 delete all
+#pm2 delete all
 
 echo "Stopping PM2 daemon..."
-pm2 kill
+#pm2 kill
 
-pm2 start npm --name "superchat" -- start 
+pm2 start npm --name "devus" -- start 
 
