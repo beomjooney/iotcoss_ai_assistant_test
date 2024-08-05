b5d07fd182fbae5d627a7ad08ebfe287e9f8dd3c
10143918 <10143918>
2024-08-05 14:09:28 +0900

    feat. 세종대 메인홈

--- src/stories/components/Header/index.tsx
+++ src/stories/components/Header/index.tsx
@@ -270,7 +270,7 @@ const Header = ({ darkBg, classOption, title, menuItem }: NavbarProps) => {
             {tenantName === 'sejong' ? (
               <img src="/assets/images/header/SEJONG_DevUs.png" width={200} alt="logo" className={cx('image-logo')} />
             ) : (
-              <img src="/assets/images/header/image_2.png" width={130} alt="logo" className={cx('image-logo')} />
+              <img src="/assets/images/header/image_1.png" width={130} alt="logo" className={cx('image-logo')} />
             )}
           </div>
           {/* {COLOR_PRESETS.map(preset => (
