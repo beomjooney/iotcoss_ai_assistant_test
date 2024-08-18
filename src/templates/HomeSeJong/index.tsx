2ef1c949894fe995dd26b4955d4caa12f0555f14
10143918 <10143918>
2024-08-18 20:58:33 +0900

    chore. sejong margint top

--- src/templates/HomeSeJong/index.tsx
+++ src/templates/HomeSeJong/index.tsx
@@ -30,7 +30,7 @@ export function HomeSejongTemplate({ logged = false, tenantName = '' }: HomeSejo
   return (
     <div className={cx('career-main')}>
       <div
-        className="tw-text-white tw-bg-[url('/assets/images/main/Union.webp')]  tw-bg-cover tw-bg-center tw-bg-no-repeat"
+        className="tw-mt-4 tw-text-white tw-bg-[url('/assets/images/main/Union.webp')]  tw-bg-cover tw-bg-center tw-bg-no-repeat"
         style={{ height: '650px' }}
       >
         <section
