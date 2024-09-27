b072c38a060d465416db4a08e5f76dd1da861808
10143918 <10143918>
2024-09-27 19:47:48 +0900

    feat. 강의클럽 enddate

---
+++ src/templates/Account/LoginOne/index.tsx
@@ -0,0 +1,246 @@
+import styles from './index.module.scss';
+import classNames from 'classnames/bind';
+import { useRouter } from 'next/router';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import Typography from '@mui/material/Typography';
+import { useLogin } from 'src/services/account/account.mutations';
+import { usePresets } from 'src/utils/color-presets';
+import { useEffect, useState } from 'react';
+import { useColorPresets } from 'src/utils/use-theme-color';
+import { useColorPresetName } from 'src/utils/use-theme-color';
+import { deleteCookie } from 'cookies-next';
+import { useSessionStore } from '../../../store/session';
+import { getFirstSubdomain } from 'src/utils';
+import { getButtonClass } from 'src/utils/clubStatus';
+interface LoginTempOnelateProps {
+  onSubmitLogin: () => void;
+}
+
+const cx = classNames.bind(styles);
+
+interface LoginTemplateProps {
+  title: string;
+  onSubmitLogin: () => void;
+}
+
+export function LoginOneTemplate({ title = '', onSubmitLogin }: LoginTempOnelateProps) {
+  const { mutate: onLogin, isSuccess, data: loginData } = useLogin();
+  const { update, tenantName } = useSessionStore.getState();
+  const router = useRouter();
+  const COLOR_PRESETS = usePresets();
+  const { setColorPresetName } = useColorPresetName();
+  const { setColorPresets } = useColorPresets();
+  const [username, setUserName] = useState('');
+
+  console.log('login join page', getFirstSubdomain(), tenantName);
+  useEffect(() => {
+    if (!COLOR_PRESETS || COLOR_PRESETS.length === 0) return;
+
+    const preset = COLOR_PRESETS.find(preset => preset.name === getFirstSubdomain()) || COLOR_PRESETS[0];
+    setColorPresetName(preset.name);
+    setColorPresets(preset.colors);
+
+    console.log(preset.name);
+    localStorage.setItem('activeIndex', '0');
+  }, []);
+
+  const [clientTenantName, setClientTenantName] = useState(null);
+  useEffect(() => {
+    // 클라이언트에서만 tenantName을 설정
+    setClientTenantName(tenantName);
+  }, []);
+
+  useEffect(() => {
+    if (isSuccess) {
+      onSubmitLogin();
+      //redirection 처리
+      update({
+        tenantName: loginData?.tenant_uri?.split('.')[0],
+        redirections: loginData?.redirections,
+        menu: loginData?.menu,
+      });
+
+      // Check if running in the local environment
+      const isLocalEnv = process.env.NEXT_PUBLIC_ENV === 'local';
+      const isLocalProd = process.env.NEXT_PUBLIC_ENV === 'prod';
+      console.log('loginData?.tenant_uri', loginData?.tenant_uri, getFirstSubdomain(), isLocalEnv);
+
+      // if (loginData?.tenant_uri === getFirstSubdomain() || isLocalEnv || isLocalProd) {
+      if (loginData?.tenant_uri === getFirstSubdomain() || isLocalEnv) {
+        location.href = '/';
+      } else {
+        const authStore = localStorage.getItem('auth-store');
+        if (authStore) {
+          const json = JSON.parse(authStore);
+          const jsonString = JSON.stringify(json.state);
+          // 1. Base64 인코딩 (Node.js 환경에서는 Buffer를 사용)
+          const encodedJson = Buffer.from(jsonString).toString('base64');
+          // location.href = loginData?.redirections?.home_url + `?authStore=${encodedJson}`;
+          deleteCookie('access_token');
+          localStorage.removeItem('auth-store');
+          localStorage.removeItem('app-storage');
+          location.href = loginData?.redirections?.home_url + `?authStore=${encodedJson}`;
+          //test
+          //location.href = 'http://dsu.localhost:3001/' + `?authStore=${encodedJson}`;
+        }
+      }
+    }
+  }, [loginData]);
+
+  const handleLogin = async () => {
+    location.href = `${process.env['NEXT_PUBLIC_GENERAL_API_URL']}/oauth2/authorize/kakao?redirect_uri=${process.env['NEXT_PUBLIC_REDIRECT_URL']}`;
+  };
+
+  const handleData = async () => {
+    onSubmitLogin && onSubmitLogin();
+  };
+
+  const validationSchema = Yup.object().shape({
+    // username: Yup.string().required('Email is required').email('Email is invalid'),
+    password: Yup.string()
+      .required('Password is required')
+      .min(4, 'Password must be at least 4 characters')
+      .max(20, 'Password must not exceed 20 characters'),
+  });
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = data => {
+    setUserName(data.username);
+    onLogin(
+      paramsWithDefault({
+        ...data,
+        tenant_uri: getFirstSubdomain(),
+      }),
+    );
+  };
+
+  const onError = (e: any) => {
+    console.log('error', e);
+  };
+
+  return (
+    <div className={cx('login-container')}>
+      <div className={cx('logo-area')}>
+        {/* <img src="/assets/images/cm_CI_co_1000x225.png" alt="footer logo" width={162} className="img-fluid mb-3" /> */}
+        <p className={cx('tw-font-bold tw-text-[26px] tw-text-black tw-py-5 tw-text-center')}>{title} 로그인</p>
+        {/* <Divider className={cx('sign-color')}>또는 이메일 로그인</Divider> */}
+      </div>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
+        <Typography sx={{ fontSize: 14, marginTop: 3, color: 'black', fontWeight: '600' }}>학번 또는 이메일</Typography>
+        <TextField
+          required
+          id="username"
+          name="username"
+          placeholder="학번 또는 이메일을 입렧해주세요."
+          variant="outlined"
+          type="search"
+          fullWidth
+          sx={{
+            backgroundColor: '#F6F7FB',
+            borderRadius: '10px',
+            marginTop: '10px',
+            '& label': { fontSize: 15, color: '#919191', fontWeight: 'light' },
+            '& fieldset': { border: 'none' },
+            '& input': { height: ' 0.8em;' },
+          }}
+          margin="dense"
+          {...register('username')}
+          error={errors.username ? true : false}
+          helperText={errors.username?.message}
+        />
+        <Typography sx={{ fontSize: 14, marginTop: 3, color: 'black', fontWeight: '600' }}>비밀번호</Typography>
+        <TextField
+          required
+          id="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요."
+          type="password"
+          fullWidth
+          margin="dense"
+          variant="outlined"
+          sx={{
+            backgroundColor: '#F6F7FB',
+            borderRadius: '10px',
+            marginTop: '10px',
+            '& fieldset': { border: 'none' },
+            '& label': { fontSize: 15, color: '#919191', fontWeight: 'light' },
+            '& input': { height: ' 0.8em;' },
+          }}
+          {...register('password')}
+          error={errors.password ? true : false}
+          helperText={errors.password?.message}
+        />
+
+        <Grid container spacing={3} direction="row" justifyContent="space-between" alignItems="center">
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}>
+            <Box
+              display="flex"
+              justifyContent="flex-end"
+              sx={{ fontWeight: 'bold' }}
+              className="tw-cursor-pointer"
+              onClick={() => router.push('/account/forgot')}
+            >
+              <Typography sx={{ fontSize: 12, textDecoration: 'underline' }} className="tw-py-3">
+                비밀번호를 잊으셨나요?
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+        <div style={{ marginBottom: '20px', marginTop: '20px', textAlign: 'center' }}>
+          <button
+            className={`${getButtonClass(
+              clientTenantName,
+            )} tw-font-bold tw-rounded-md tw-w-full tw-h-[48px] tw-text-white`}
+            onClick={() => handleSubmit(onSubmit)}
+          >
+            로그인
+          </button>
+        </div>
+      </form>
+      <Divider className={cx('sign-color', 'tw-py-3')}>또는</Divider>
+      <div style={{ marginBottom: '20px', marginTop: '20px' }}>
+        <button className="tw-font-bold tw-rounded-md tw-w-full tw-h-[48px] tw-bg-white border tw-text-black">
+          <Typography sx={{ fontWeight: '600', fontSize: 16 }}>학번으로 로그인</Typography>
+        </button>
+      </div>
+      <Box display="flex" justifyContent="center" sx={{ fontWeight: 'bold' }}>
+        <Typography
+          onClick={() => router.push('/account/signup')}
+          sx={{ fontSize: 14, textDecoration: 'underline' }}
+          className="tw-py-3  tw-cursor-pointer"
+        >
+          회원가입
+        </Typography>
+      </Box>
+    </div>
+  );
+}
+
+export default LoginOneTemplate;
+const paramsWithDefault = (params: any) => {
+  const defaultParams: any = {
+    username: '',
+    password: '',
+    grant_type: 'password',
+    tenant_uri: '',
+  };
+  return {
+    ...defaultParams,
+    ...params,
+  };
+};
---
+++ src/templates/Quiz/ManageQuizClub/group.js
@@ -0,0 +1,219 @@
+// groups.js
+
+export const dayGroup = [
+  {
+    id: '월',
+    groupId: '0001',
+    name: '월',
+    description: '월',
+    order: 1,
+  },
+  {
+    id: '화',
+    groupId: '0001',
+    name: '화',
+    description: '화',
+    order: 2,
+  },
+  {
+    id: '수',
+    groupId: '0001',
+    name: '수',
+    description: '수',
+    order: 3,
+  },
+  {
+    id: '목',
+    groupId: '0001',
+    name: '목',
+    description: '목',
+    order: 3,
+  },
+  {
+    id: '금',
+    groupId: '0001',
+    name: '금',
+    description: '금',
+    order: 3,
+  },
+  {
+    id: '토',
+    groupId: '0001',
+    name: '토',
+    description: '토',
+    order: 4,
+  },
+  {
+    id: '일',
+    groupId: '0001',
+    name: '일',
+    description: '일',
+    order: 4,
+  },
+];
+
+export const scheduleDataDummy = [
+  {
+    order: 1,
+    weekNumber: 1,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 2,
+    weekNumber: 2,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 3,
+    weekNumber: 3,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 4,
+    weekNumber: 4,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 5,
+    weekNumber: 5,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 6,
+    weekNumber: 6,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 7,
+    weekNumber: 7,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 8,
+    weekNumber: 8,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 9,
+    weekNumber: 9,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 10,
+    weekNumber: 10,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 11,
+    weekNumber: 11,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+  {
+    order: 12,
+    weekNumber: 12,
+    publishDate: null,
+    dayOfWeek: null,
+    quizSequence: null,
+  },
+];
+
+export const images = [
+  '/assets/images/banner/Rectangle_190.png',
+  '/assets/images/banner/Rectangle_191.png',
+  '/assets/images/banner/Rectangle_192.png',
+  '/assets/images/banner/Rectangle_193.png',
+  '/assets/images/banner/Rectangle_195.png',
+  '/assets/images/banner/Rectangle_196.png',
+  '/assets/images/banner/Rectangle_197.png',
+  '/assets/images/banner/Rectangle_198.png',
+];
+
+export const imageBanner = [
+  '/assets/images/banner/Rectangle_200.png',
+  '/assets/images/banner/Rectangle_201.png',
+  '/assets/images/banner/Rectangle_202.png',
+  '/assets/images/banner/Rectangle_203.png',
+];
+
+export const privateGroup = [
+  {
+    id: '0100',
+    groupId: '0001',
+    name: '공개',
+    description: '공개',
+    active: true,
+    order: 1,
+  },
+  {
+    id: '0200',
+    groupId: '0001',
+    name: '비공개',
+    description: '비공개',
+    active: false,
+    order: 2,
+  },
+];
+
+export const levelGroup = [
+  {
+    name: '0',
+    description: '1학년',
+  },
+  {
+    name: '1',
+    description: '2학년',
+  },
+  {
+    name: '2',
+    description: '3학년',
+  },
+  {
+    name: '3',
+    description: '4학년',
+  },
+  {
+    name: '4',
+    description: '취업준비생',
+  },
+  {
+    name: '5',
+    description: '상관없음',
+  },
+];
+
+export const openGroup = [
+  {
+    name: '0100',
+    description: '정기 자동 오픈',
+  },
+  {
+    name: '0200',
+    description: '교수자 수동 오픈',
+  },
+  {
+    name: '0300',
+    description: '학습자 학습 오픈',
+  },
+];
