4fc99fe7825bbc96cfaa29ed3801a8a20410e120
kimcy <kimcy1@gmail.com>
2024-10-27 00:20:58 +0900

    fix. 관리자 페이지 강의 등록 추가

--- src/templates/System/Admin/index.tsx
+++ src/templates/System/Admin/index.tsx
@@ -239,7 +239,7 @@ export function AdminTemplate({ children }: AdminTemplateProps) {
                 <div className="tw-p-5">
                   <div className="tw-text-lg tw-pb-4 tw-font-semibold tw-text-black">
                     {/* 안녕하세요! {summary?.member?.nickname}님 */}
-                    안녕하세요! 대학관리자님
+                    안녕하세요! 관리자님
                   </div>
                 </div>
               </div>
