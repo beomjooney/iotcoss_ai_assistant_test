328b1856fa7128fa95788aff00339ad3248fd45a
kimcy <kimcy1@gmail.com>
2025-05-24 23:22:40 +0900

    feat: 파일 다운로드 방식 개선 및 PDF 파일 처리 로직 추가

--- src/stories/components/QuizClubDetaillSolution/index.tsx
+++ src/stories/components/QuizClubDetaillSolution/index.tsx
@@ -852,8 +852,11 @@ const QuizClubDetaillSolution = ({
                                     <div className="tw-flex tw-justify-end tw-items-center tw-relative tw-gap-2 tw-px-2 tw-py-1 tw-rounded">
                                       <button
                                         onClick={() => {
-                                          onFileDownload(item?.file?.key, item?.file?.name);
-                                          // window.open(item?.contentUrl, '_blank'); // data?.articleUrl을 새 탭으로 열기
+                                          if (item?.file?.name?.toLowerCase().endsWith('.pdf')) {
+                                            onFileDownload(item?.file?.key, item?.file?.name);
+                                          } else {
+                                            window.open(item?.contentUrl, '_blank'); // data?.articleUrl을 새 탭으로 열기
+                                          }
                                         }}
                                         className="tw-bg-black tw-p-1.5 tw-text-white tw-rounded tw-flex-grow-0 tw-flex-shrink-0 tw-text-xs tw-font-bold tw-text-right tw-text-[#9ca5b2]"
                                       >
