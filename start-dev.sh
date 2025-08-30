b862fc88f72e0bceb971790a6ef8e4b3239da989
root <root@ip-172-31-48-195.ap-northeast-2.compute.internal>
2025-08-30 20:00:23 +0900

    chore : add file

---
+++ start-dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git pull origin main
+yarn build-dev
+#pm2 delete pnpm build
+
+echo "Stopping all PM2 processes..."
+#pm2 stop all
+
+echo "Deleting all PM2 processes..."
+#pm2 delete all
+
+echo "Stopping PM2 daemon..."
+#pm2 kill
+
+pm2 stop "devus"
+pm2 start yarn --name "devus" -- start-dev
+
