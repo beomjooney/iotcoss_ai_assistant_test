29780342378b8f71fad2d00f45b10ade222d6ba9
kimcy <kimcy1@gmail.com>
2025-09-14 01:41:02 +0900

    fix: 강의 종료일 검증 로직 수정으로 사용자 오류 안내 개선

--- src/templates/Lecture/Open/index.tsx
+++ src/templates/Lecture/Open/index.tsx
@@ -1262,11 +1262,11 @@ export function LectureOpenTemplate() {
         const nextDay4 = dayjs(item.endDate);
 
         // 시작일이 종료일보다 크거나 같을 경우 오류 처리
-        // if (!dayjs(nextDay4).isAfter(dayjs(nextDay3))) {
-        //   alert(`${i + 1}번째 강의 : 종료일은 시작일보다 뒤에 있어야 합니다.`);
-        //   setIsProcessing(false);
-        //   return; // 혹은 필요에 따라 validation 실패시 코드 실행 중단
-        // }
+        if (dayjs(nextDay4).isBefore(dayjs(nextDay3), 'day')) {
+          alert(`${i + 1}번째 강의 : 종료일은 시작일보다 이전일 수 없습니다.`);
+          setIsProcessing(false);
+          return; // 혹은 필요에 따라 validation 실패시 코드 실행 중단
+        }
 
         // 중복된 날짜 검사
         // for (let prev of previousSchedules) {
