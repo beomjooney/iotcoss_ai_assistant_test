0ea951951c4b9b9b42cf54d71ce07746b2e598e6
kimcy <kimcy1@gmail.com>
2025-09-13 20:11:08 +0900

    fix: 강의 카드 및 상세 솔루션 컴포넌트에서 불필요한 코드 제거 및 조건부 렌더링 개선으로 코드 가독성 향상

--- src/stories/components/LectureCard/index.tsx
+++ src/stories/components/LectureCard/index.tsx
@@ -1,14 +1,12 @@
 import classNames from 'classnames/bind';
 import styles from './index.module.scss';
-import { BoardType } from 'src/config/entities';
 import React, { useEffect, useRef, useState } from 'react';
-import { User } from 'src/models/user';
-import { useSaveLike, useDeleteLike, useSaveReply, useDeleteReply } from 'src/services/community/community.mutations';
+import { useSaveLike, useDeleteLike } from 'src/services/community/community.mutations';
 import { useSessionStore } from 'src/store/session';
 import Grid from '@mui/material/Grid';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import StarIcon from '@mui/icons-material/Star';
-import { Desktop, Mobile } from 'src/hooks/mediaQuery';
+import { Desktop } from 'src/hooks/mediaQuery';
 
 export interface LectureCardProps {
   /** 게시판 object */
@@ -19,8 +17,6 @@ export interface LectureCardProps {
   selectedImage: string;
   selectedImageBanner: string;
 }
-
-const cx = classNames.bind(styles);
 
 const LectureCard = ({
   item,
@@ -69,14 +65,6 @@ LectureCardProps) => {
       <Desktop>
         <Grid item xs={xs}>
           <a
-            // href={logged ? '/lecture/' + `${item.clubSequence}` : '#'}
-            // href={
-            //   logged
-            //     ? roles?.includes('ROLE_ADMIN') || (roles?.includes('ROLE_MANAGER') && memberId === item.leaderUUID)
-            //       ? '/lecture-dashboard/' + `${item.clubSequence}`
-            //       : '/lecture/' + `${item.clubSequence}`
-            //     : '#'
-            // }
             href={
               logged
                 ? item.isClubAdmin
@@ -104,33 +92,43 @@ LectureCardProps) => {
             )}
             <div
               style={{ backgroundImage: `url(${item.backgroundImage || '/assets/images/banner/Rectangle_200.png'})` }}
-              className="border  tw-h-[235px] tw-relative tw-overflow-hidden tw-rounded-lg  tw-bg-cover tw-bg-no-repeat tw-bg-center tw-border tw-border-[#e9ecf2]"
+              className="tw-p-5 border tw-h-[235px] tw-relative tw-overflow-hidden tw-rounded-2xl  tw-bg-cover tw-bg-no-repeat tw-bg-center"
             >
-              <div className="tw-absolute tw-inset-0 tw-bg-white tw-opacity-90 tw-rounded-lg md:tw-rounded-none md:tw-rounded-r-lg"></div>
-              <svg
-                width={320}
-                height={320}
-                viewBox="0 0 275 240"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-                className="tw-absolute tw-left-[268px] tw-top-[-44px] tw-opacity-50"
-                preserveAspectRatio="xMidYMid meet"
-              >
-                <circle opacity="0.5" cx={163} cy={120} r={163} fill="url(#paint0_linear_3000_19130)" />
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_3000_19130"
-                    x1={163}
-                    y1={-43}
-                    x2={163}
-                    y2={283}
-                    gradientUnits="userSpaceOnUse"
+              <div className="tw-absolute tw-inset-0 tw-bg-white tw-opacity-90"></div>
+
+              {item.leaderProfileImageUrl && (
+                <div>
+                  <svg
+                    width={320}
+                    height={320}
+                    viewBox="0 0 275 240"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="tw-absolute tw-left-[268px] tw-top-[-44px] tw-opacity-50"
+                    preserveAspectRatio="xMidYMid meet"
                   >
-                    <stop stopColor="white" stopOpacity={0} />
-                    <stop offset={1} stopColor="#2A46AC" stopOpacity="0.5" />
-                  </linearGradient>
-                </defs>
-              </svg>
+                    <circle opacity="0.5" cx={163} cy={120} r={163} fill="url(#paint0_linear_3000_19130)" />
+                    <defs>
+                      <linearGradient
+                        id="paint0_linear_3000_19130"
+                        x1={163}
+                        y1={-43}
+                        x2={163}
+                        y2={283}
+                        gradientUnits="userSpaceOnUse"
+                      >
+                        <stop stopColor="white" stopOpacity={0} />
+                        <stop offset={1} stopColor="#2A46AC" stopOpacity="0.5" />
+                      </linearGradient>
+                    </defs>
+                  </svg>
+                  <img
+                    alt="leaderProfileImageUrl"
+                    className="tw-absolute tw-left-[329.5px] tw-top-[17.5px] tw-w-[200px] tw-h-[200px] tw-rounded-full"
+                    src={item.leaderProfileImageUrl || '/assets/images/account/default_profile_image.png'}
+                  />
+                </div>
+              )}
               <div className=" tw-h-60 tw-absolute tw-left-[-1px] tw-top-[-1px]" />
               <button
                 className="tw-w-6 tw-h-6 tw-absolute tw-left-[504px] tw-top-4"
@@ -155,40 +153,31 @@ LectureCardProps) => {
                 </span>
               </p>
               <div className="tw-flex tw-justify-start tw-items-start tw-absolute tw-left-5 tw-top-5 tw-gap-1">
-                <div className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#e4e4e4]">
-                  <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#5f5f5f]">
-                    {item?.jobGroups[0]?.name || 'N/A'}
-                  </p>
-                </div>
+                {item?.jobGroups?.[0]?.name && (
+                  <div className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#e4e4e4]">
+                    <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#5f5f5f]">
+                      {item.jobGroups[0].name}
+                    </p>
+                  </div>
+                )}
 
-                {/* {item?.jobs?.length > 0 &&
-                  item.jobs.map((job, index) => ( */}
-                <div
-                  // key={index}
-                  className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#d7ecff]"
-                >
-                  <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#235a8d]">
-                    {item?.jobs[0]?.name || 'N/A'}
-                  </p>
-                </div>
-                {/* ))} */}
+                {item?.jobs?.[0]?.name && (
+                  <div className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#d7ecff]">
+                    <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#235a8d]">
+                      {item.jobs[0].name}
+                    </p>
+                  </div>
+                )}
 
-                {/* {item?.jobLevels?.length > 0 &&
-                  item.jobLevels.map((jobLevel, index) => ( */}
-                <div
-                  // key={index}
-                  className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#fffdc8]"
-                >
-                  <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#806024]">
-                    {item?.jobLevels?.[0]?.name || 'N/A'}
-                  </p>
-                </div>
-                {/* ))} */}
+                {item?.jobLevels?.[0]?.name && (
+                  <div className="tw-flex tw-justify-start tw-items-center tw-flex-grow-0 tw-flex-shrink-0 tw-relative tw-gap-2.5 tw-px-2 tw-py-0.5 tw-rounded tw-bg-[#fffdc8]">
+                    <p className="tw-flex-grow-0 tw-flex-shrink-0 tw-text-sm tw-text-left tw-text-[#806024]">
+                      {item.jobLevels[0].name}
+                    </p>
+                  </div>
+                )}
               </div>
-              <img
-                className="tw-absolute tw-left-[329.5px] tw-top-[17.5px] tw-w-[200px] tw-h-[200px] tw-rounded-full"
-                src={item.leaderProfileImageUrl || '/assets/images/account/default_profile_image.png'}
-              />
+
               <p className="tw-absolute tw-left-5 tw-top-[202px] tw-text-sm tw-font-bold tw-text-left tw-text-black">
                 {item.leaderNickname}
               </p>
--- src/stories/components/LectureDetaillSolution/index.tsx
+++ src/stories/components/LectureDetaillSolution/index.tsx
@@ -189,12 +189,14 @@ const LectureDetaillSolution = ({
         >
           <Grid container direction="row" justifyContent="space-between" alignItems="start" rowSpacing={0}>
             <Grid item xs={8}>
-              <div className="tw-gap-3  tw-flex tw-items-center tw-flex-wrap tw-text-base tw-mb-0 tw-text-sm tw-font-normal tw-text-gray-500 dark:tw-text-gray-400">
-                <div className="tw-bg-[#d7ecff] tw-rounded-[3.5px] tw-px-2 tw-py-[1px]">
-                  <p className="tw-text-[12.25px] tw-text-[#235a8d]">
-                    {contents?.jobGroups && contents?.jobGroups.length > 0 ? contents?.jobGroups[0]?.name : ''}
-                  </p>
-                </div>
+              <div className="tw-gap-3  tw-flex tw-items-center tw-flex-wrap tw-mb-0 tw-text-sm tw-font-normal tw-text-gray-500 dark:tw-text-gray-400">
+                {contents?.jobGroups?.[0]?.name && (
+                  <div className="tw-bg-[#d7ecff] tw-rounded-[3.5px] tw-px-2 tw-py-[1px]">
+                    <p className="tw-text-[12.25px] tw-text-[#235a8d]">
+                      {contents?.jobGroups && contents?.jobGroups.length > 0 ? contents?.jobGroups[0]?.name : ''}
+                    </p>
+                  </div>
+                )}
                 {contents?.jobs?.length > 0 &&
                   contents?.jobs.map((job, index) => (
                     <div key={index} className="tw-bg-[#ffdede] tw-rounded-[3.5px] tw-px-2 tw-py-[1px]">
